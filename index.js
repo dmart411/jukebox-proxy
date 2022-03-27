f59ae08953a9f44cf5613320f6923bc13c670478
Daniel Martinez <danielm@ornsoft.com>
2022-03-27 11:39:07 -0400

    albums and songs routes

--- index.js
+++ index.js
@@ -8,9 +8,30 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req,res) => {
-    res.send('hi');
-})
+app.get("/albums", (req, res) => {
+  request(
+    { url: "https://stg-resque.hakuapp.com/albums.json" },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: "error", message: err.message });
+      }
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+app.get("/songs/:album", (req, res) => {
+    const albumId = req.params.album
+    request(
+      { url: `https://stg-resque.hakuapp.com/songs.json?album_id=${albumId}` },
+      (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          return res.status(500).json({ type: "error", message: err.message });
+        }
+        res.json(JSON.parse(body));
+      }
+    );
+  });
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
