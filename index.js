b9f7ce372ac0660d21bfaf094f7c7b89032cba36
Daniel Martinez <danielm@ornsoft.com>
2022-03-27 11:41:10 -0400

    style

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ app.get("/albums", (req, res) => {
 });
 
 app.get("/songs/:album", (req, res) => {
-    const albumId = req.params.album
-    request(
-      { url: `https://stg-resque.hakuapp.com/songs.json?album_id=${albumId}` },
-      (error, response, body) => {
-        if (error || response.statusCode !== 200) {
-          return res.status(500).json({ type: "error", message: err.message });
-        }
-        res.json(JSON.parse(body));
+  const albumId = req.params.album;
+  request(
+    { url: `https://stg-resque.hakuapp.com/songs.json?album_id=${albumId}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: "error", message: err.message });
       }
-    );
-  });
+      res.json(JSON.parse(body));
+    }
+  );
+});
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
